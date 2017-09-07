dd8f2cac89e4579c8428de45c3f44b8fa602470c
afeiship <1290657123@qq.com>
2017-09-07 11:33:16 +0800

    issue: pass

--- test/test.js
+++ test/test.js
@@ -7,14 +7,55 @@ describe('next-treemap', function () {
 
   var items = require('./test.json');
 
-  it('nx.treeMap', function () {
+  it('nx.treeMap - big data', function () {
     var rs = nx.treeMap( items,{
       label:'HYMC',
       value:'HY_DM',
       children:'children'
     });
 
-    console.log(rs);
+    // console.log(rs);
+  });
+
+
+  it('nx.treeMap - simple', function () {
+    var data = [{
+      key: 'zhejiang',
+      name: 'Zhejiang',
+      child: [{
+        key: 'hangzhou',
+        name: 'Hangzhou',
+        child: [{
+          key: 'xihu',
+          name: 'West Lake',
+        }],
+      }],
+    }, {
+      key: 'jiangsu',
+      name: 'Jiangsu',
+      child: [{
+        key: 'nanjing',
+        name: 'Nanjing',
+        child: [{
+          key: 'zhonghuamen',
+          name: 'Zhong Hua Men',
+        }],
+      }],
+    }];
+    var rs = nx.treeMap( data,{
+      label:'name',
+      value:'key',
+      children:'child'
+    });
+
+    assert.deepEqual(rs[0].children,[{
+      value: 'hangzhou',
+      label: 'Hangzhou',
+      children: [{
+        value: 'xihu',
+        label: 'West Lake',
+      }],
+    }] );
   });
 
 });
